bf0ac4d44ef5333cf8b96a82eb4e84f23a13eac5
Clément Joly <clement.joly@telecomnancy.eu>
2018-04-06 06:13:29 +0200

    Factor random matrix generation
    Do not check if allocated matrix is not NULL as it is not done in the doc.

--- src/factorisation.c
+++ src/factorisation.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 #include <gsl/gsl_matrix.h>
 
+gsl_matrix* gen_random_matrix(int nb_line, int nb_col) {
+  gsl_matrix* random_matrix = gsl_matrix_alloc(nb_line, nb_col);
+  srand(6); // Initialize with a number for reproductability
+  for (int line=0; line<nb_line; line++) {
+    for (int col=0; col<nb_col; col++) {
+      gsl_matrix_set(random_matrix, line, col, rand()/10e4);
+    }
+  }
+  return random_matrix;
+}
+
 int main() {
   printf("Printing a random matrix\n");
 
   int size = 10;
-  gsl_matrix* random_matrix = gsl_matrix_alloc(size, size);
-  if (random_matrix == NULL) return 1;
-
-  srand(6); // Initialize with a number for reproductability
-  for (int line=0; line<size; line++) {
-    for (int col=0; col<size; col++) {
-      gsl_matrix_set(random_matrix, line, col, rand()/10e4);
-    }
-  }
+  gsl_matrix* random_matrix = gen_random_matrix(size, size);
 
   gsl_matrix_fprintf(stdout, random_matrix, "%f");
 
