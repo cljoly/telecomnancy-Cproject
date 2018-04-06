13b8a2b98ccfeeebb56817b7d3992473a21a4f81
Clément Joly <clement.joly@telecomnancy.eu>
2018-04-06 06:20:43 +0200

    Auto-format code

--- src/factorisation.c
+++ src/factorisation.c
@@ -1,13 +1,13 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <gsl/gsl_matrix.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-gsl_matrix* gen_random_matrix(int nb_line, int nb_col) {
-  gsl_matrix* random_matrix = gsl_matrix_alloc(nb_line, nb_col);
+gsl_matrix *gen_random_matrix(int nb_line, int nb_col) {
+  gsl_matrix *random_matrix = gsl_matrix_alloc(nb_line, nb_col);
   srand(6); // Initialize with a number for reproductability
-  for (int line=0; line<nb_line; line++) {
-    for (int col=0; col<nb_col; col++) {
-      gsl_matrix_set(random_matrix, line, col, rand()/10e4);
+  for (int line = 0; line < nb_line; line++) {
+    for (int col = 0; col < nb_col; col++) {
+      gsl_matrix_set(random_matrix, line, col, rand() / 10e4);
     }
   }
   return random_matrix;
@@ -17,7 +17,7 @@ int main() {
   printf("Printing a random matrix\n");
 
   int size = 10;
-  gsl_matrix* random_matrix = gen_random_matrix(size, size);
+  gsl_matrix *random_matrix = gen_random_matrix(size, size);
 
   gsl_matrix_fprintf(stdout, random_matrix, "%f");
 
