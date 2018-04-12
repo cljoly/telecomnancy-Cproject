be3a492094fa0074224ceaa2d578b676e1b62187
Clément Joly <clement.joly@telecomnancy.eu>
2018-04-12 16:09:34 +0200

    Traduction en français des commentaires

--- src/factorisation.c
+++ src/factorisation.c
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Generate random matrix, filled with mark like data, in [1,5] */
+/* Création d’une matrice remplie de notes aléatoires, dans [1,5] */
 gsl_matrix *gen_random_matrix(int nb_row, int nb_col) {
   gsl_matrix *random_matrix = gsl_matrix_alloc(nb_row, nb_col);
-  srand(6); // Initialize with a number for reproductability
+  // XXX Initialisation avec un nombre pour pouvoir reproduire les tests avec
+  // les mêmes erreurs
+  srand(6);
   for (int row = 0; row < nb_row; row++) {
     for (int col = 0; col < nb_col; col++) {
       gsl_matrix_set(random_matrix, row, col, (rand() % 5) + 1);
@@ -16,9 +18,10 @@ gsl_matrix *gen_random_matrix(int nb_row, int nb_col) {
   return random_matrix;
 }
 
-// Based on http://bit.ly/2qbhehb, same notation
-/* In place modify P and Q, so that P and Q are a factorisation of R. K is the
-   number of latent feature */
+// Basé sur http://bit.ly/2qbhehb, avec les mêmes notations
+/* Modification en place de P et Q, de manière à ce que P et Q constituent une
+   factorisation approximative de R.
+   K est le nombre de critères latents. */
 void factor(gsl_matrix *R, gsl_matrix *P, gsl_matrix *Q, int K) {
   int steps = 5000;
   double alpha = 0.0002;
@@ -28,8 +31,8 @@ void factor(gsl_matrix *R, gsl_matrix *P, gsl_matrix *Q, int K) {
   double r_ij;
   double e_ij;
   double e;
-  gsl_vector *Q_row_i = gsl_vector_alloc(R->size2); // size2: number of column
-  gsl_vector *P_col_j = gsl_vector_alloc(R->size1); // size1: number of row
+  gsl_vector *Q_row_i = gsl_vector_alloc(R->size2);
+  gsl_vector *P_col_j = gsl_vector_alloc(R->size1);
   double *pik;
   double *qkj;
   while (--steps > 0) {
@@ -39,7 +42,7 @@ void factor(gsl_matrix *R, gsl_matrix *P, gsl_matrix *Q, int K) {
         if (r_ij > 0) {
           gsl_matrix_get_row(Q_row_i, Q, i);
           gsl_matrix_get_col(P_col_j, P, j);
-          // ddot is less precise than dsdot, but should be enough
+          // ddot est moins précis que dsdot, mais cela devrait suffire
           gsl_blas_ddot(Q_row_i, P_col_j, &e_ij);
           e_ij = r_ij - e_ij;
           for (int k = 0; k < K; k++) {
@@ -59,7 +62,7 @@ void factor(gsl_matrix *R, gsl_matrix *P, gsl_matrix *Q, int K) {
         if (r_ij > 0) {
           gsl_matrix_get_row(Q_row_i, Q, i);
           gsl_matrix_get_col(P_col_j, P, j);
-          // ddot is less precise than dsdot, but should be enough
+          // ddot est moins précis que dsdot, mais cela devrait suffire
           gsl_blas_ddot(Q_row_i, P_col_j, &e_ij);
           e_ij = r_ij - e_ij;
           e = e + e_ij * e_ij;
