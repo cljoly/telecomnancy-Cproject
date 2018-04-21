4c8d016ffb272374bc2115c54877e1d77f3eab93
Clément Joly <clement.joly@telecomnancy.eu>
2018-04-21 18:41:28 +0200

    Indentation automatique

--- src/factorisation.c
+++ src/factorisation.c
@@ -54,13 +54,13 @@ static void factor_walker(void (*g)(factor_context *ctxt),
 
       if (r_ij > 0) {
         if (DEBUG)
-        printf("a\n");
+          printf("a\n");
         gsl_matrix_get_row(ctxt->Q_row_i, ctxt->Q, i);
         if (DEBUG)
-        printf("a\n");
+          printf("a\n");
         gsl_matrix_get_col(ctxt->P_col_j, ctxt->P, j);
         if (DEBUG)
-        printf("a\n");
+          printf("a\n");
 
         double e_ij;
         // ddot est moins précis que dsdot, mais cela devrait suffire
@@ -69,13 +69,13 @@ static void factor_walker(void (*g)(factor_context *ctxt),
 
         for (int k = 0; k < ctxt->K; k++) {
           if (DEBUG)
-          printf("b\n");
+            printf("b\n");
           double *p_ik = gsl_matrix_ptr(ctxt->P, i, k);
           if (DEBUG)
-          printf("b\n");
+            printf("b\n");
           double *q_kj = gsl_matrix_ptr(ctxt->Q, k, j);
           if (DEBUG)
-          printf("b\n");
+            printf("b\n");
 
           f(p_ik, q_kj, ctxt);
         }
